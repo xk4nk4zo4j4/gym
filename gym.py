aa10510c995e289446bb64e125e12dcfc180f174
xk4nk4zo4j4 <48575819+xk4nk4zo4j4@users.noreply.github.com>
2019-03-27 22:19:27 +0800

    Update gym.py

--- gym.py
+++ gym.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 import requests
 import json
-
 import threading
+import datetime
+import time
+
+currentDT = datetime.datetime.now()
 
 def printGym():
-    threading.Timer(1.0, printGym).start()
+    threading.Timer(900, printGym).start()
     r = requests.get("https://dasc.cyc.org.tw/api")
     fields = json.loads(r.text)
 
     gym_on = fields["gym"][0]
     swim_on = fields["swim"][0]
 
-    print(gym_on, swim_on)
+    print(gym_on, swim_on,str(currentDT))
 
 printGym()
